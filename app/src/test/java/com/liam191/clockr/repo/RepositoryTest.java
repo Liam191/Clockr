dc196ab4af4a45caa79f01aab5d28c4962c44710
Liam Hilliard <liam.h.-@hotmail.com>
2020-03-15 21:35:28 +0000

    Add Repository test class

---
+++ app/src/test/java/com/liam191/clockr/repo/RepositoryTest.java
@@ -0,0 +1,10 @@
+package com.liam191.clockr.repo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class RepositoryTest {
+    @Test
+    public void testRepository_(){}
+}
