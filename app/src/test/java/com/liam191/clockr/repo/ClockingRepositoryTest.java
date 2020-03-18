5f2cf78638feadcce11aabd8cb64469740a0cb20
Liam Hilliard <liam.h.-@hotmail.com>
2020-03-18 22:43:02 +0000

    Change SDK version to v24

---
+++ app/src/test/java/com/liam191/clockr/repo/ClockingRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.liam191.clockr.repo;
+
+import com.liam191.clockr.clocking.Clocking;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ClockingRepositoryTest {
+    @Test
+    public void testClockingRepo_getClockingsForGivenDay(){
+        //LiveData<List<Clocking>> clockings = new ClockingRepository().getClockingsForDay
+    }
+}
