a439b0d5e5a86d86593d939d422e5e540ca9e51c
Liam Hilliard <liam.h.-@hotmail.com>
2020-05-01 23:48:34 +0100

    Extract InstantExecutorExtension into separate file

--- app/src/test/java/com/liam191/clockr/repo/ClockingRepositoryTest.java
+++ app/src/test/java/com/liam191/clockr/repo/ClockingRepositoryTest.java
@@ -3,17 +3,11 @@ package com.liam191.clockr.repo;
 import com.liam191.clockr.clocking.Clocking;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.AfterEachCallback;
-import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.ExtensionContext;
 
 import java.util.Date;
 import java.util.List;
 
-import androidx.annotation.NonNull;
-import androidx.arch.core.executor.ArchTaskExecutor;
-import androidx.arch.core.executor.TaskExecutor;
 import androidx.lifecycle.MutableLiveData;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -49,20 +43,3 @@ public class ClockingRepositoryTest {
         assertTrue(repository.getAllForDate(testDay).getValue().contains(clocking));
     }
 }
-
-class InstantExecutorExtension implements BeforeEachCallback, AfterEachCallback{
-    @Override
-    public void beforeEach(ExtensionContext context) throws Exception {
-        ArchTaskExecutor.getInstance().setDelegate(new TaskExecutor() {
-            @Override public void executeOnDiskIO(@NonNull Runnable runnable) { runnable.run(); }
-            @Override public void postToMainThread(@NonNull Runnable runnable) { runnable.run(); }
-            @Override public boolean isMainThread() { return true; }
-        });
-    }
-
-    @Override
-    public void afterEach(ExtensionContext context) throws Exception {
-        ArchTaskExecutor.getInstance().setDelegate(null);
-    }
-}
-
---
+++ app/src/test/java/com/liam191/clockr/repo/InstantExecutorExtension.java
@@ -0,0 +1,25 @@
+package com.liam191.clockr.repo;
+
+import androidx.annotation.NonNull;
+import androidx.arch.core.executor.ArchTaskExecutor;
+import androidx.arch.core.executor.TaskExecutor;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+class InstantExecutorExtension implements BeforeEachCallback, AfterEachCallback {
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+        ArchTaskExecutor.getInstance().setDelegate(new TaskExecutor() {
+            @Override public void executeOnDiskIO(@NonNull Runnable runnable) { runnable.run(); }
+            @Override public void postToMainThread(@NonNull Runnable runnable) { runnable.run(); }
+            @Override public boolean isMainThread() { return true; }
+        });
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        ArchTaskExecutor.getInstance().setDelegate(null);
+    }
+}
