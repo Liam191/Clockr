f7166765edc59b68ef9d2c5387ca40cb5b28fe9e
Liam Hilliard <liam.h.-@hotmail.com>
2020-05-08 05:06:36 +0100

    Implement equals and hashcode methods

--- app/src/main/java/com/liam191/clockr/repo/db/ClockingEntity.java
+++ app/src/main/java/com/liam191/clockr/repo/db/ClockingEntity.java
@@ -1,11 +1,13 @@
 package com.liam191.clockr.repo.db;
 
+import java.util.Objects;
+
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity
-class ClockingEntity {
+public class ClockingEntity {
     @PrimaryKey(autoGenerate = true)
     int uid;
 
@@ -20,4 +22,26 @@ class ClockingEntity {
 
     @ColumnInfo(name = "start_time")
     Long startTime;
+
+    @Override
+    public boolean equals(Object o){
+        if(o == null || o.getClass() != getClass()){
+            return false;
+        }
+        ClockingEntity entity = (ClockingEntity) o;
+        return (
+                label.equals(entity.label) &&
+                        description.equals(entity.description) &&
+                        durationInMinutes == entity.durationInMinutes &&
+                        startTime.equals(entity.startTime));
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(
+                this.label,
+                this.description,
+                this.durationInMinutes,
+                this.startTime);
+    }
 }
