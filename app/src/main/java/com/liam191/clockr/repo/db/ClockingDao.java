90dcffa62e14df7d0cb0e07f2c16e1029499ad52
Liam Hilliard <liam.h.-@hotmail.com>
2020-05-08 01:26:17 +0100

    Add explicit "public abstract" to DAO methods

--- app/src/main/java/com/liam191/clockr/repo/db/ClockingDao.java
+++ app/src/main/java/com/liam191/clockr/repo/db/ClockingDao.java
@@ -1,19 +1,19 @@
 package com.liam191.clockr.repo.db;
 
-import java.util.List;
-
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
 
+import java.util.List;
+
 @Dao
 public interface ClockingDao {
     @Insert
-    void add(ClockingEntity clocking);
+    public abstract void add(ClockingEntity clocking);
 
     @Query("SELECT * FROM clockingentity")
-    List<ClockingEntity> getAll();
+    public abstract List<ClockingEntity> getAll();
 
     @Query("DELETE FROM clockingentity")
-    void clear();
+    public abstract void clear();
 }
