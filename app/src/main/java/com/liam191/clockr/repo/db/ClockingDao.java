4470bfd587eb3eb857eb6674dc0b75085315de5f
Liam Hilliard <liam.h.-@hotmail.com>
2020-05-08 05:04:58 +0100

    Remove TypeConverter annotation from database and DAO

--- app/src/main/java/com/liam191/clockr/repo/db/ClockingDao.java
+++ app/src/main/java/com/liam191/clockr/repo/db/ClockingDao.java
@@ -1,20 +1,18 @@
 package com.liam191.clockr.repo.db;
 
+import java.util.List;
+
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
-import androidx.room.TypeConverters;
-
-import com.liam191.clockr.clocking.Clocking;
 
 @Dao
 public interface ClockingDao {
     @Insert
-    @TypeConverters({ClockingConverter.class})
-    public abstract void add(Clocking clocking);
+    public abstract void add(ClockingEntity clocking);
 
-    //@Query("SELECT * FROM clockingentity")
-    //public abstract List<Clocking> getAll();
+    @Query("SELECT * FROM clockingentity")
+    public abstract List<ClockingEntity> getAll();
 
     @Query("DELETE FROM clockingentity")
     public abstract void clear();
--- app/src/main/java/com/liam191/clockr/repo/db/ClockrDatabase.java
+++ app/src/main/java/com/liam191/clockr/repo/db/ClockrDatabase.java
@@ -2,10 +2,8 @@ package com.liam191.clockr.repo.db;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-import androidx.room.TypeConverters;
 
 @Database(entities = {ClockingEntity.class}, version = 2)
-@TypeConverters({ClockingConverter.class})
 public abstract class ClockrDatabase extends RoomDatabase {
     public abstract ClockingDao clockingDao();
 }
