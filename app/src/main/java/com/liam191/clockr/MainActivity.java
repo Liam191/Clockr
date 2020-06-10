f81f4efcc7734e8b49e55e79094c52be97c253c2
Liam Hilliard <liam.h.-@hotmail.com>
2020-06-10 01:19:43 +0100

    Add styling to RecyclerView

--- app/src/main/java/com/liam191/clockr/MainActivity.java
+++ app/src/main/java/com/liam191/clockr/MainActivity.java
@@ -1,10 +1,11 @@
 package com.liam191.clockr;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.liam191.clockr.clocking.Clocking;
@@ -12,9 +13,12 @@ import com.liam191.clockr.repo.ClockingDayViewModel;
 
 import org.threeten.bp.Clock;
 import org.threeten.bp.ZonedDateTime;
+import org.threeten.bp.format.DateTimeFormatter;
+import org.threeten.bp.format.FormatStyle;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -73,13 +77,15 @@ public class MainActivity extends AppCompatActivity {
         private class ClockingViewHolder extends RecyclerView.ViewHolder {
             public TextView clockingLabel;
             public TextView clockingDescription;
-            public EditText clockingStartDate;
+            public TextView clockingStartDate;
+            public ImageView icon;
 
             public ClockingViewHolder(View itemView) {
                 super(itemView);
-                this.clockingLabel = itemView.findViewById(R.id.clocking_label);
-                this.clockingDescription = itemView.findViewById(R.id.clocking_description);
-                this.clockingStartDate = itemView.findViewById(R.id.clocking_startdate);
+                this.clockingLabel = itemView.findViewById(R.id.label);
+                this.clockingDescription = itemView.findViewById(R.id.description);
+                this.clockingStartDate = itemView.findViewById(R.id.start_time);
+                this.icon = itemView.findViewById(R.id.icon);
             }
         }
 
@@ -95,8 +101,20 @@ public class MainActivity extends AppCompatActivity {
         public void onBindViewHolder(@NonNull ClockingViewHolder holder, int position) {
             Clocking clocking = clockingList.get(position);
             holder.clockingLabel.setText(clocking.label());
-            holder.clockingDescription.setText(clocking.description());
-            holder.clockingStartDate.setText(clocking.startTime().toString());
+            holder.clockingStartDate.setText(clocking.startTime().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)));
+
+            if(clocking.description().equals("")){
+                // Hide this view if there's nothing to display.
+                // We set height to 0 over visibility.GONE so the constraints of the layout still work
+                holder.clockingDescription.setHeight(0);
+            } else {
+                holder.clockingDescription.setText(clocking.description());
+            }
+
+            Random rnd = new Random();
+            holder.icon.setColorFilter(Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)));
+
+
         }
 
         @Override
