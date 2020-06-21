768fa039caa4a934d77da53b9548e7ae63debde9
Liam Hilliard <liam.h.-@hotmail.com>
2020-06-21 02:02:40 +0100

    Rename DayActivity to MainActivity

--- app/src/main/java/com/liam191/clockr/gui/MainActivity.java
+++ app/src/main/java/com/liam191/clockr/gui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.liam191.clockr.gui.dayview;
+package com.liam191.clockr.gui;
 
 import android.os.Bundle;
 
@@ -8,11 +8,11 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.Navigation;
 
-public class DayActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_day);
+        setContentView(R.layout.activity_main);
         Navigation.findNavController(findViewById(R.id.nav_host_fragment)).navigate(R.id.dayListFragment);
     }
 }
