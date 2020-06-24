d37bfd3389d5daec6076fe32006e0000deeddd88
Liam Hilliard <liam.h.-@hotmail.com>
2020-06-24 22:59:20 +0100

    Remove LiveData from ClockingDayDao

--- app/src/main/java/com/liam191/clockr/repo/db/ClockingDayDao.java
+++ app/src/main/java/com/liam191/clockr/repo/db/ClockingDayDao.java
@@ -1,13 +1,12 @@
 package com.liam191.clockr.repo.db;
 
-import androidx.lifecycle.LiveData;
-import androidx.room.Dao;
-import androidx.room.Query;
-import androidx.room.TypeConverters;
-
 import org.threeten.bp.ZonedDateTime;
 
 import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Query;
+import androidx.room.TypeConverters;
 
 @Dao
 public interface ClockingDayDao {
@@ -15,5 +14,5 @@ public interface ClockingDayDao {
             " WHERE date(substr(:date, 0, instr(:date, '['))) = date(substr(start_time, 0, instr(start_time, '[')))" +
             " OR date(substr(:date, 0, instr(:date, '['))) = date(substr(end_time, 0, instr(end_time, '[')))")
     @TypeConverters(ZonedDateTimeConverter.class)
-    LiveData<List<ClockingEntity>> getAllForDate(ZonedDateTime date);
+    List<ClockingEntity> getAllForDate(ZonedDateTime date);
 }
